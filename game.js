89bf0e46d99822373a23623c27787dc97b902fd4
AEiOuf <65119959+AEiOuf@users.noreply.github.com>
2020-09-29 10:19:54 +0800

    stuff

--- game.js
+++ game.js
@@ -1,19 +1,33 @@
-var gameData = {
+var player = {
     number: 0,
-    incrementPerClick: 1,
-    incrementCost: 10
-  }
-
-function increment() {
-    gameData.number += gameData.incrementPerClick
-    document.getElementById("numberThing").innerHTML = gameData.number
+    increment: 1,
+    firstCost: 10,
+    incremented: false
 }
-function buyIncrementIncrementer(){
-    if (gameData.number >= gameData.incrementCost){
-        gameData.number -= gameData.incrementCost
-        gameData.incrementCost *= 5
-        gameData.incrementPerClick *= 3
-        document.getElementById("numberThing").innerHTML = gameData.number
-        document.getElementById("incrementIncrementerThing").innerHTML = "Make number go up even more | Cost: " + gameData.incrementCost
+
+function goUp() {
+    player.number += player.increment
+    document.getElementById("number").innerHTML = "You have " + player.number + "."
+    player.incremented = true
+    if (player.incremented){
+        document.getElementById("lol").style.display = "block"
     }
 }
+
+
+function squarecrement(){
+    if(player.number >= player.firstCost){
+        player.number -= player.firstCost
+        player.increment *= 2
+        player.firstCost *= 3
+        document.getElementById("squarecrementd").innerHTML = "Upgrade increment power | Cost: " + player.firstCost
+        document.getElementById("lol12").innerHTML = `Your increment power is currently ${player.increment}.`
+    }
+}
+
+var mainGameLoop = window.setInterval(function() {
+    if(player.number >= 10){
+        document.getElementById("lol2").style.display = "block"
+    }
+    document.getElementById("number").innerHTML = "You have " + player.number + "."
+}, 100)
